3f475ab68526dc257a16bbabd37839f4f799df90
morisliem <morisliem8@gmail.com>
2021-10-01 11:47:06 +0800

    change the workspace

---
+++ src/activity.go
@@ -0,0 +1,28 @@
+package src
+
+type Activity struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Label       string `json:"label"`
+	Deadline    string `json:"deadline"`
+	Severity    string `json:"severity"`
+	Priority    string `json:"priority"`
+}
+
+type ActivityConstructer struct {
+	Activities []Activity
+}
+
+func NewActivity() *ActivityConstructer {
+	return &ActivityConstructer{
+		Activities: []Activity{},
+	}
+}
+
+func (r *ActivityConstructer) AddActivity(activity Activity) {
+	r.Activities = append(r.Activities, activity)
+}
+
+func (r *ActivityConstructer) GetActivities() []Activity {
+	return r.Activities
+}
