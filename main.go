c4bcafecfaf0a465d9a8d66f7f72a628ff4dee5f
morisliem <morisliem8@gmail.com>
2021-10-04 23:42:50 +0700

    add login and logout

--- main.go
+++ main.go
@@ -32,8 +32,43 @@ func main() {
 		res, err := src.AddUser(ctx, rdb, newUser)
 
 		if err != nil {
+			w.WriteHeader(400)
 			json.NewEncoder(w).Encode(res)
 		}
+		w.WriteHeader(201)
+		json.NewEncoder(w).Encode(res)
+	})
+
+	router.Post("/login", func(w http.ResponseWriter, r *http.Request) {
+		request := map[string]string{}
+		json.NewDecoder(r.Body).Decode(&request)
+
+		login := src.User{
+			Username: request["username"],
+			Password: request["password"],
+		}
+
+		res, err := src.LoginUser(ctx, rdb, login)
+
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(res)
+		}
+
+		w.WriteHeader(200)
+		json.NewEncoder(w).Encode(res)
+
+	})
+
+	router.Post("/{username}/logout", func(w http.ResponseWriter, r *http.Request) {
+		username := chi.URLParam(r, "username")
+		res, err := src.LogoutUser(ctx, rdb, username)
+
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(res)
+		}
+		w.WriteHeader(200)
 		json.NewEncoder(w).Encode(res)
 	})
 
@@ -43,8 +78,10 @@ func main() {
 		res, err := src.GetUser(ctx, rdb, username)
 
 		if err != nil {
+			w.WriteHeader(404)
 			json.NewEncoder(w).Encode(res["Message"])
 		}
+		w.WriteHeader(200)
 		json.NewEncoder(w).Encode(res)
 	})
 
--- src/user.go
+++ src/user.go
@@ -32,6 +32,7 @@ func AddUser(ctx context.Context, db *redis.Client, usr User) (map[string]string
 		result["Message"] = "Failed to add"
 		return result, err
 	}
+
 	result["Message"] = "Successfully added"
 	return result, nil
 }
@@ -41,6 +42,7 @@ func GetUser(ctx context.Context, db *redis.Client, usr string) (map[string]stri
 	if err != nil {
 		panic(err)
 	}
+
 	result := map[string]string{}
 	if len(value) == 0 {
 		result["Message"] = "User not found"
@@ -51,5 +53,30 @@ func GetUser(ctx context.Context, db *redis.Client, usr string) (map[string]stri
 		result[key] = val
 	}
 
+	return result, nil
+}
+
+func LoginUser(ctx context.Context, db *redis.Client, usr User) (map[string]string, error) {
+	password, err := db.HGet(ctx, usr.Username, "password").Result()
+	result := map[string]string{}
+
+	if err != nil {
+		result["Message"] = "User not found"
+		return result, err
+	}
+
+	if password != usr.Password {
+		result["Message"] = "Invalid password"
+		return result, nil
+	}
+
+	result["Message"] = "Logged in successfully"
+	return result, nil
+}
+
+func LogoutUser(ctx context.Context, db *redis.Client, usr string) (map[string]string, error) {
+	result := map[string]string{}
+
+	result["Message"] = "Logged out successfully"
 	return result, nil
 }
